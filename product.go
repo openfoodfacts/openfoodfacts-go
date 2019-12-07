a8534a803d9a8af80e8a3bd7be0efcacdebfd246
areontar <areontar@localhost.localdomain>
2019-12-06 22:50:57 -0800

    fix ingrentient type from String to int

--- product.go
+++ product.go
@@ -81,7 +81,7 @@ type Product struct {
 	IngredientsFromPalmOilNumber                int            `json:"ingredients_from_palm_oil_n"`
 	IngredientsFromPalmOilTags                  []interface{}  `json:"ingredients_from_palm_oil_tags"`
 	IngredientsIdsDebug                         []string       `json:"ingredients_ids_debug"`
-	IngredientsN                                string         `json:"ingredients_n"`
+	IngredientsN                                int            `json:"ingredients_n"`
 	IngredientsNTags                            []string       `json:"ingredients_n_tags"`
 	IngredientsTags                             []string       `json:"ingredients_tags"`
 	IngredientsText                             string         `json:"ingredients_text"`
