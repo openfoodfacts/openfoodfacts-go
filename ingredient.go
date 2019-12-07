d1949c52d5b956f340e8f9bac7617f2747e4df08
areontar <areontar@localhost.localdomain>
2019-12-06 23:32:07 -0800

    added missing fields

--- ingredient.go
+++ ingredient.go
@@ -4,7 +4,10 @@
 package openfoodfacts
 
 type Ingredient struct {
-	ID   string `json:"id"`
-	Rank int    `json:"rank,omitempty"`
-	Text string `json:"text"`
+	ID         string `json:"id"`
+	Rank       int    `json:"rank,omitempty"`
+	Text       string `json:"text"`
+	Vegan      string `json:"vegan,omitempty"`
+	Vegetarian string `json:"vegetarian,omitempty"`
+	Percent    string `json:"percent,omitempty"`
 }
